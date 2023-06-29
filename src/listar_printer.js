53b5df50f435fbacd146e96e85fb774e4ce2a87b
axealan <44321538+axealan@users.noreply.github.com>
2023-06-29 10:18:36 -0300

    Update listar_printer.js

--- src/listar_printer.js
+++ src/listar_printer.js
@@ -1,33 +1,14 @@
-const express = require('express');
-const { exec } = require('child_process');
+const printer = require('printer');
 
-const app = express();
+// Lista as impressoras disponíveis
+function listarImpressoras() {
+  const impressoras = printer.getPrinters();
 
-app.get('/', function(req, res) {
-  exec('wmic printer get name', function(error, stdout, stderr) {
-    if (error) {
-      console.error('Erro ao executar o comando:', error);
-      res.status(500).json({ error: 'Erro ao executar o comando' });
-      return;
-    }
-
-    if (stderr) {
-      console.error('Erro na saída do comando:', stderr);
-      res.status(500).json({ error: 'Erro na saída do comando' });
-      return;
-    }
-
-    const printerNames = stdout.split('\n').slice(1, -1).map(name => name.trim());
-
-    console.log('Impressoras disponíveis:');
-    printerNames.forEach(function(printerName) {
-      console.log(printerName);
-    });
-
-    res.json({ printers: printerNames });
+  console.log('Impressoras disponíveis:');
+  impressoras.forEach(impressora => {
+    console.log(`- ${impressora.name}`);
   });
-});
+}
 
-app.listen(3000, function() {
-  console.log('Servidor em execução no localhost:3000');
-});
+// Executa a função para listar as impressoras
+listarImpressoras();
